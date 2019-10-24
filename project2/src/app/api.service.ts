334a1d7c9f2aa982e26b38650c4a46c968f3fd8e
SarahMac11 <s.mclaughlin51@yahoo.com>
2019-10-24 14:35:48 -0500

    inital file

---
+++ project2/src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getRepos(){
+    console.log("called getRepos");
+    return this.httpClient.get('http://augur.osshealth.io:5000/api/unstable/repos');
+  }
+}
