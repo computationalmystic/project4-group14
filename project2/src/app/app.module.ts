054b70f5820b611fb75cea6c7e3524ea48bfd904
SarahMac11 <s.mclaughlin51@yahoo.com>
2019-10-24 20:23:25 -0500

    nav bar

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      // Home Page with Repos
+      { path: '', component: AppComponent }
+      // Page with total commits for each project and proportion of commits each contributor is responsible for.
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
