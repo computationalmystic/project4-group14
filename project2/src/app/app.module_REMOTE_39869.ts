b17853a1f87848185c9b38d9239d7f06bb88dfae
Natsukie <aproditeloveu@gmail.com>
2019-10-29 06:33:10 -0500

    final version

---
+++ project2/src/app/app.module_REMOTE_39869.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { PullsComponent } from './pulls/pulls.component';
+import { RepoComponent } from './repo/repo.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ContributorsComponent } from './contributors/contributors.component';
+import { CommitsComponent } from './commits/commits.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    PullsComponent,
+    RepoComponent,
+    GroupsComponent,
+    ContributorsComponent,
+    CommitsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      // Home Page with Repos
+      { path: '', component: AppComponent }
+      // Page with total commits for each project and proportion of commits each contributor is responsible for.
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
